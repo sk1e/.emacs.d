decfc2e2307fc65731a665b3dcd5c11085dc2604
Sergey Petrov <sekk1e@yandex.ru>
2021-07-08 19:55:50 +0700

    disable backup files

--- init.el
+++ init.el
@@ -45,6 +45,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(make-backup-files nil)
  '(safe-local-variable-values '((git-commit-major-mode . git-commit-elisp-text-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
